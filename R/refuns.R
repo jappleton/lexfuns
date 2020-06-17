83c0b0fe6edcdd96c27ef04dcc2435635cab319a
Matthew Lovelace <matthew.lovelace@gcpsk12.org>
2020-06-17 04:41:20 -0400

    Minor edits to function documentation.

--- R/refuns.R
+++ R/refuns.R
@@ -7,8 +7,8 @@
 #' @examples
 #' \dontrun{
 #' born <- "1981/02/25"
-#' today <- Sys.Date()
-#' calc_age_in_yrs(born, today)
+#' yesterday <- Sys.Date() - 1
+#' calc_age_in_yrs(born, yesterday)
 #' }
 calc_age_in_yrs <- function(birth_dt, as_of_dt) {
   x_lt = as.POSIXlt(birth_dt)
@@ -37,7 +37,7 @@ calc_age_in_yrs <- function(birth_dt, as_of_dt) {
 #' \dontrun{
 #' circle_data <- circle_fun(c(1,-1), 2.3, npoints = 100)
 #' library("ggplot2")
-#' ggplot(dat,aes(x,y)) + geom_path()
+#' ggplot(circle_data, aes(x,y)) + geom_path()
 #' }
 circle_fun <- function(center = c(0, 0),
                       radius = 2,
@@ -109,7 +109,7 @@ detach_all_pkgs <- function() {
 
 #' @title (Install and) load packages.
 #'
-#' @description This function checks if each package that needs to be loaded has been installed installed, installs any that are not, and then loads all of the listed packages.
+#' @description This function checks if each package that needs to be loaded has been installed, installs any that are not, and then loads all of the listed packages.
 #' @param pkgs_needed Input a character vector of the packages you want to install/load. Can be a single package (e.g., "dplyr") or multiple (c("dplyr", "tidyr")).
 #' @export
 #' @examples
@@ -135,9 +135,17 @@ install_and_load_pkgs <- function(pkgs_needed) {
 #' @description Based on the Stata function of the same name, this function asserts whether rows of a dataframe are unique, and if they are not, it will throw an error and, when sourced, will stop the sourced script code from running any further.This function converts character and factor variables to numeric, or leaves the variable as is if it is already numeric. If the variable is some other type, the function will throw an error and, if the script is being sourced, stop the script from running further.
 #' @param x Input the ID to be checked for duplicates. Can be a single variable, or multiple.
 #' @export
+#' #' @examples
+#' \dontrun{
+#' # Works well with dplyr::select, especially when id consists of multiple columns
+#' library(dplyr)
+#' fake_data <- data.frame(x = c(1, 1, 2, 2, 3, 3), y = c('A', 'B', 'A', 'B', 'A', 'B'), z = c(47, 32, 64, 64, 42, 47))
+#' isid(fake_data %>% select(x))
+#' isid(fake_data %>% select(x, y))
+#' }
 isid <- function(x) {
-  if (sum(duplicated(x)) > 0) stop('Nope. Duplicates.')
-  if (sum(duplicated(x)) == 0) return('Yep!')
+  if (sum(duplicated(x)) > 0) stop('Nope. There are duplicates in this id.')
+  if (sum(duplicated(x)) == 0) return('Yep! This is a unique id.')
   stopifnot(sum(duplicated(x)) == 0)
 }
 
@@ -165,21 +173,18 @@ rad2deg <-
 #'
 #' @description This function rounds numbers in the same way that Excel does:
 #'
-#' Rounds up the number line when values are positive and trailing value is 5-9.
+#' Rounds up the number line when values are positive and trailing value is 5 through 9.
 #'
-#' Rounds down the number line when values are negative and trailing value is 5-9.
+#' Rounds down the number line when values are negative and trailing value is 5 through 9.
 #'
-#' If a trailing value is 5 exactly, always rounds in the same direction (i.e., up the number
+#' If a trailing value is 5 exactly, this function always rounds in the same direction (i.e., up the number
 #' line when value is positive, down the number line when value is negative). This is unlike
-#' R which uses a more statistically valid but less widely accepted rounding rule of randomly
+#' R's round() function which uses a more statistically valid but not as common rounding rule of randomly
 #' rounding half of the exact 5s up and half of the exact 5s down.
 #'
-#' Solves floating point rounding errors that seem to occur at a rate of about 1/200 or so,
-#' in which a value that looks like an exact 5 (e.g., 0.5) is stored with some "decimal dust"
-#' (e.g., looks like 0.5 but is really 0.4999999999999999) because, long story short,
-#' computers are weird.
+#' It also resolves floating point rounding issues that would otherwise occur from time to time (this is
+#' why there is a 0.00000001 added/subtracted to the value prior to rounding).
 #'
-#' Would not solve bit flips caused by cosmic rays, though. Which is a very rare, but real thing. Look it up.
 #' @param val Input the value to be rounded.
 #' @param n_dec Input the number of decimal places to round to (e.g., 0 = round to integer, 1 = round to the tenths place, 2 = round to the hundreths place, etc.).
 #' @export
